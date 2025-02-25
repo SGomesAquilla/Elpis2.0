8d0496d94faeb3387599be3f52bbe14c3985245c
AquillaSG <aquillagomes@hotmail.com>
2025-02-25 16:11:58 -0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<script setup>
+    // import logos from assets/img/..
+</script>
+
+<template>
+    <header>
+    <div class="menu">
+      <nav>
+        <a href=""><img class="elpisL" alt="Home"></a>
+        <button class="navButton" onclick="readingPage()">
+          <img class="navIconR" alt="Reading"> Reading
+        </button>
+        <button class="navButton" onclick="songPage()">
+          <img class="navIconS" alt="Songs"> Songs
+        </button>
+        <button class="navButton" onclick="communityPage()">
+          <img class="navIconC" alt="Community"> Community
+        </button>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style>
+    
+</style>
