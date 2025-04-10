e79fc292526ba887569db41699ae63e9d4c905fc
AquillaSG <aquillagomes@hotmail.com>
2025-04-10 11:14:09 -0300

    fixed value type error inside HomeView and added .env

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import Navbar from '@/components/Navbar.vue';
   import Foot from '@/components/Foot.vue';
   import { RouterView } from 'vue-router';
+  console.log(import.meta.env.VITE_GOOGLE_API_KEY);
 </script>
 
 <template>
